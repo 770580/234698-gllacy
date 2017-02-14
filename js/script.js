76629c46d40a9c492fe7d21d59e5da510553fe79
Anton <lascivolin@gmail.com>
2017-02-15 01:43:15 +0300

    закончил верстку всплывающего окна

--- js/script.js
+++ js/script.js
@@ -1,28 +1,16 @@
-const feedbackOpenButton = document.querySelector('.feedback-button');
-const feedbackPopUp = document.querySelector('.feedback');
-const feedbackCloseButton = document.querySelector('.feedback-close');
-
-function getCoords(elem) {
-  const box = elem.getBoundingClientRect();
-
-  return {
-    top: box.top + pageYOffset,
-    left: box.left + pageXOffset
-  };
-}
+var feedbackOpenButton = document.querySelector('.feedback-button');
+var feedbackOverlay = document.querySelector('.feedback-overlay');
+var feedbackCloseButton = document.querySelector('.feedback-close');
 
 function openFeedbackWindow(event) {
   event.preventDefault();
-  feedbackPopUp.classList.remove('feedback-hidden');
+  feedbackOverlay.classList.remove('feedback-hidden');
 }
 
 function closeFeedbackWindow(event) {
   event.preventDefault();
-  feedbackPopUp.classList.add('feedback-hidden');
+  feedbackOverlay.classList.add('feedback-hidden');
 }
 
 feedbackOpenButton.addEventListener('click', this.openFeedbackWindow);
 feedbackCloseButton.addEventListener('click', this.closeFeedbackWindow);
-
-feedbackPopUp.style.top = getCoords(feedbackPopUp).top + 'px';
-feedbackPopUp.style.left = getCoords(feedbackPopUp).left + 'px';
